4eeb92f29547afd4393f37b748a7457c7940972d
Yauheni Kapachenia <kapachenia@gmail.com>
2021-09-23 11:09:37 +0300

    hw1 analyze the video

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -2,21 +2,16 @@ import React from 'react'
 import {MessageDataType} from "./HW1";
 import s from "./Message.module.css"
 
-
 function Message(props: MessageDataType) {
     return (
-        <div className={s.content}>
-            <img src={props.avatar} />
-            <div className={s.commentBubble}>
-                <div className={s.contentMessage}>
-                    <div className={s.name}> { props.name } </div>
-                    <div className={s.message}> { props.message } </div>
-                </div>
-                <div className={s.dataTime}>
-                    {props.time}
-                </div>
+        <div className={s.message}>
+            <img alt={'avatar'} src={props.avatar}/>
+            <div className={s.angle}></div>
+            <div className={s.content}>
+                <div className={s.name}> {props.name} </div>
+                <div className={s.text}> {props.message} </div>
+                <div className={s.time}> {props.time} </div>
             </div>
-
         </div>
     )
 }
